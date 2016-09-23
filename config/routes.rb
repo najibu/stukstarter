6afca58c87683058239afc359c61671d6a86ff0a
najibu <ns.najibu@gmail.com>
2016-09-23 18:37:02 +0100

    Create reward controller new form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'projects#index'
-  resources :projects
+
+  resources :projects do 
+  	resources :rewards, only: [:new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
