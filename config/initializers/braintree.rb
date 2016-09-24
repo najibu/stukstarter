aabdc4dfd5e9d7a842d13fafa8bdb55322081253
najibu <ns.najibu@gmail.com>
2016-09-24 18:40:02 +0100

    Installed braintree and created payment cv

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "zgfvtdfr4q24327s"
+Braintree::Configuration.public_key = "zh63xs4h3jwr67vk"
+Braintree::Configuration.private_key = "54ac86d4fd98850e81c0b8d4f81748b0"
